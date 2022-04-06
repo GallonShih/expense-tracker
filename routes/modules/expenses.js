a5accad25f1bfedb39b613d776675741e9a9399a
GallonShih <Gallon.Shih@testritegroup.com>
2022-04-06 13:57:08 +0800

    feet: add edit page & submit form

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,12 +1,14 @@
 // require relative packages
 const express = require('express')
 const router = express.Router()
+const moment = require('moment')
 
 // require relative js files
 const Category = require('../../models/category')
 const Record = require('../../models/record')
 
 // setting router
+// new expense page
 router.get('/new', (req, res) => {
   Category.find()
     .lean()
@@ -21,6 +23,36 @@ router.post('/', (req, res) => {
   return Record.create(expenseNew)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+})
+// edit expense page
+router.get('/:id/edit', (req, res) => {
+  const _id = req.params.id
+  return Record.findById({ _id })
+    .populate({ path: 'categoryId' })
+    .lean()
+    .then((recordData) => {
+      recordData.date = moment(recordData.date).format('YYYY-MM-DD')
+      Category.find()
+        .lean()
+        .sort({ sort_no: 'asc' }) // asc/desc
+        .then(categoryData => {
+          categoryData.forEach((category) => {
+            if (category._id.equals(recordData.categoryId._id)) {
+              category.isSelected = true
+            } else {
+              category.isSelected = false
+            }
+          })
+          return res.render('edit', { recordData, categoryData })
+        })
+    })
+})
+// submitting edit page
+router.put('/:id', (req, res) => {
+  const _id = req.params.id
+  Record.findByIdAndUpdate(_id, req.body)
+    .then(() => res.redirect(`/`))
+    .catch(err => console.log(err))
 })
 
 // export
