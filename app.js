351b1707e149b7c78a4a70e22dafa9f9d1163158
GallonShih <Gallon.Shih@testritegroup.com>
2022-04-07 15:43:49 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require relative packages
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -18,6 +19,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 
