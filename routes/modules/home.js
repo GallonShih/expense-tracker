bbd0d35be28e1fa3776767f71a8921fdfa5e41c3
GallonShih <Gallon.Shih@testritegroup.com>
2022-04-07 11:54:00 +0800

    feet: add category filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -23,6 +23,61 @@ router.get('/', (req, res) => {
         .lean()
         .sort({ sort_no: 'asc' }) // asc/desc
         .then(categoryData => {
+          categoryData.unshift(
+            {
+              _id: new Object(''),
+              name: '類別',
+              isSelected: true,
+              isDisabled: true
+            },
+            {
+              _id: new Object('all'),
+              name: '所有'
+            }
+          )
+          return res.render('index', { recordData, totalAmount, categoryData })
+        })
+    })
+    .catch(error => console.error(error))
+})
+// category filter
+router.get('/filter', (req, res) => {
+  const recordFilter = {}
+  const categoryId = req.query.categoryId
+  if (req.query.categoryId !== 'all') {
+    recordFilter.categoryId = categoryId
+  }
+  Record.find(recordFilter)
+    .populate({ path: 'categoryId' })
+    .lean()
+    .sort({ date: 'desc' }) // asc/desc
+    .then(recordData => {
+      let totalAmount = 0
+      recordData.forEach(record => {
+        record.date = moment(record.date).format('YYYY/MM/DD')
+        totalAmount += record.amount
+      })
+      Category.find()
+        .lean()
+        .sort({ sort_no: 'asc' }) // asc/desc
+        .then(categoryData => {
+          categoryData.forEach((category) => {
+            if (category._id.equals(categoryId)) {
+              category.isSelected = true
+            }
+          })
+          categoryData.unshift(
+            {
+              _id: new Object(''),
+              name: '類別',
+              isDisabled: true
+            },
+            {
+              _id: new Object('all'),
+              name: '所有',
+              isSelected: true
+            }
+          )
           return res.render('index', { recordData, totalAmount, categoryData })
         })
     })
